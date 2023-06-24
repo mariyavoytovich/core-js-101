11549747942219a2e6bac397b79639ff993d9a33
mariyavoytovich <68275286+mariyavoytovich@users.noreply.github.com>
2023-06-24 23:26:39 +0300

    feat: add implementation of objects tasks

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,12 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width: width,
+    height: height,
+    getArea: () => width * height
+  }
 }
 
 
@@ -35,8 +39,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +55,12 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  const values = Object.values(obj);
+
+  return new proto.constructor(...values);
+
 }
 
 
@@ -109,34 +117,154 @@ function fromJSON(/* proto, json */) {
  *
  *  For more examples see unit tests.
  */
+class CombineSelectorBuilder{
+  constructor(selector1, combinator, selector2){
+    this._selector1 = selector1;
+    this._combinator = combinator;
+    this._selector2 = selector2;
+  }
+
+  stringify(){
+    return `${this._selector1.stringify()} ${this._combinator} ${this._selector2.stringify()}`;
+  }
+}
+
+const ELEMENTS = {
+  TAG: 'TAG',
+  ID: 'ID',
+  CLASS: 'CLASS',
+  ATTRIBUTE: 'ATTRIBUTE',
+  PSEUDO_CLASS: 'PSEUDO-CLASS',
+  PSEUDO_ELEMENT: 'PSEUDO-ELEMENT'
+}
+
+class SelectorBuilder{
+  constructor() {
+    this._elementsPathOrder = [
+      ELEMENTS.TAG,
+      ELEMENTS.ID,
+      ELEMENTS.CLASS,
+      ELEMENTS.ATTRIBUTE,
+      ELEMENTS.PSEUDO_CLASS,
+      ELEMENTS.PSEUDO_ELEMENT];
+
+    this._map = new Map();
+  }
+
+  element(value) {
+    this.checkElementExist();
+    this.checkElementOrder(ELEMENTS.TAG);
+    this._map.set(ELEMENTS.TAG, value);
+    return this;
+  }
+
+
+  id(value) {
+    this.checkIdExist();
+    this.checkElementOrder(ELEMENTS.ID);
+    this._map.set(ELEMENTS.ID, `#${value}`);
+    return this;
+  }
+
+  class(value) {
+    this.checkElementOrder(ELEMENTS.CLASS);
+    this.setArrayValueToMap(ELEMENTS.CLASS, `.${value}`);
+    return this;
+  }
+
+  attr(value) {
+    this.checkElementOrder(ELEMENTS.ATTRIBUTE);
+    this.setArrayValueToMap(ELEMENTS.ATTRIBUTE, `[${value}]`);
+    return this;
+  }
+
+  pseudoClass(value) {
+    this.checkElementOrder(ELEMENTS.PSEUDO_CLASS);
+    this.setArrayValueToMap(ELEMENTS.PSEUDO_CLASS, `:${value}`);
+    return this;
+  }
+
+  pseudoElement(value) {
+    this.checkPseudoElementExist();
+    this.checkElementOrder(ELEMENTS.PSEUDO_ELEMENT);
+    this._map.set(ELEMENTS.PSEUDO_ELEMENT, `::${value}`);
+    return this;
+  }
+
+  stringify(){
+    let result = '';
+
+    for(let value of this._map.values()){
+      result += Array.isArray(value) ? value.join('') : value;
+    }
+    return result;
+  }
+
+  setArrayValueToMap(key, value){
+    const elements = this._map.get(key);
+
+    (elements)
+      ? (elements.push(value))
+      : (this._map.set(key, [value]));
+  }
+
+  checkPseudoElementExist(){
+    if(this._map.has(ELEMENTS.PSEUDO_ELEMENT))
+      throw new Error("Element, id and pseudo-element should not occur more then one time inside the selector");
+  }
+
+  checkIdExist(){
+    if(this._map.has(ELEMENTS.ID))
+      throw new Error("Element, id and pseudo-element should not occur more then one time inside the selector");
+  }
+
+  checkElementExist(){
+    if(this._map.has(ELEMENTS.TAG))
+      throw new Error("Element, id and pseudo-element should not occur more then one time inside the selector");
+  }
+
+  checkElementOrder(element){
+    const index = this._elementsPathOrder.indexOf(element);
+    for(let i = index + 1; i < this._elementsPathOrder.length; i++){
+      if(this._map.has(this._elementsPathOrder[i]))
+        this.throwPathException();
+    }
+  }
+
+  throwPathException(){
+    throw new Error("Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element");
+  }
+}
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  elements: [],
+
+  element(value) {
+    return new SelectorBuilder().element(value);
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    return new SelectorBuilder().id(value)
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    return new SelectorBuilder().class(value)
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    return new SelectorBuilder().attr(value)
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    return new SelectorBuilder().pseudoClass(value)
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    return new SelectorBuilder().pseudoElement(value)
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2 ) {
+    return new CombineSelectorBuilder(selector1, combinator, selector2);
   },
 };
 
